7030929291e2759ed0c0636b1c1b3ef8f697a2b5
Karim Jaouhar <karimjawhar5@gmail.com>
2025-04-12 19:04:40 -0400

    Revised Heading elements not in a sequentially-descending order

--- app/components/PortfolioItem.tsx
+++ app/components/PortfolioItem.tsx
@@ -26,9 +26,9 @@ const PortfolioItem: React.FC<PortfolioItemProps> = ({
       className="w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-105"
     />
     <div className="absolute bottom-0 left-0 w-full p-4">
-      <h4 className="text-xl text-white transition duration-300">
+      <h3 className="text-xl text-white transition duration-300">
         {title}
-      </h4>
+      </h3>
       <p className="text-sm font-extralight text-gray-100">{subtitle}</p>
     </div>
   </Link>
