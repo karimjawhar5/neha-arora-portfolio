831cc034cc0a5cb6a842d1e50cca8dbd465c90b6
Karim Jaouhar <karimjawhar5@gmail.com>
2025-04-12 05:12:59 -0400

    Added a link to home for logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,12 +2,13 @@
 
 import React from "react";
 import NavigationMenu from "./NavigationMenu";
+import Link from "next/link";
 
 const Header: React.FC = () => {
   return (
     <header className="flex justify-between items-center px-6 md:px-12 py-6">
       <div>
-        <h1 className="font-light text-3xl md:text-4xl">NEHA ARORA</h1>
+        <h1 className="font-light text-3xl md:text-4xl"><Link href={"/"}>NEHA ARORA</Link></h1>
         <p className="font-light text-sm md:text-md">DIRECTOR/ PRODUCTION DESIGNER</p>
       </div>
       <NavigationMenu />
